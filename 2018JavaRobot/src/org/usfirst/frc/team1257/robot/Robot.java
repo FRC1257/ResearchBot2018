a0032f3649b64f1aa1be7235ec4eec7c36ee53a4
Adam Zamlynny <azamlynny@hotmail.com>
2018-05-24 17:56:12 -0400

    Iterative Java code was made neat and intuitive comments were removed.

--- 2018JavaRobot/src/org/usfirst/frc/team1257/robot/Robot.java
+++ 2018JavaRobot/src/org/usfirst/frc/team1257/robot/Robot.java
@@ -1,36 +1,23 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team1257.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	
 	XboxController DriveController = new XboxController(0);
+	
 	WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(1);
 	WPI_TalonSRX m_rearLeft = new WPI_TalonSRX(2);
 	SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
@@ -41,10 +28,6 @@ public class Robot extends IterativeRobot {
 
 	DifferentialDrive DriveTrain = new DifferentialDrive(m_left, m_right);
 	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
@@ -71,9 +54,6 @@ public class Robot extends IterativeRobot {
 		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		switch (m_autoSelected) {
@@ -87,9 +67,9 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
+	
+	
+	
 	@Override
 	public void teleopPeriodic() {
 		Drive();
@@ -125,9 +105,8 @@ public class Robot extends IterativeRobot {
 		DriveTrain.arcadeDrive(-forwardSpeed, turnSpeed);
 	}
 	
-	/**
-	 * This function is called periodically during test mode.
-	 */
+
+	
 	@Override
 	public void testPeriodic() {
 	}
